44ce96357e18c78f51b67bf22a242857278da8a3
HikingCarrot7 <ricardoibarra2044@gmail.com>
2022-05-22 17:03:08 -0500

    chapter: about_triangle_project
    chapter: about_triangle_project

--- koans/triangle.rb
+++ koans/triangle.rb
@@ -14,7 +14,30 @@
 #   about_triangle_project_2.rb
 #
 def triangle(a, b, c)
-  # WRITE THIS CODE
+  if valid_side_lengths?(a, b, c) and valid_triangle?(a, b, c)
+    return :equilateral if equilateral?(a, b, c)
+    return :scalene if scalene?(a, b, c)
+
+    :isosceles
+  else
+    raise TriangleError
+  end
+end
+
+def equilateral?(a, b, c)
+  [a, b, c].uniq.size == 1
+end
+
+def scalene?(a, b, c)
+  [a, b, c].uniq.size == 3
+end
+
+def valid_side_lengths?(a, b, c)
+  [a, b, c].all? { |x| x > 0 }
+end
+
+def valid_triangle?(a, b, c)
+  a + b > c && a + c > b && b + c > a
 end
 
 # Error class used in part 2.  No need to change this code.
